08e28f1c684f288eaad3cc9095e7f6129092cd26
LegendaVselennoy <rom.ahmedhanov@yandex.ru>
2024-06-15 19:19:22 +0300

    Revert "Cancle"
    This reverts commit 297c8e9a24fb1e4ad52ea2b24c0f7cb3de8fc29a.

--- src/main/java/ch1/Main.java
+++ src/main/java/ch1/Main.java
@@ -1,14 +1,12 @@
 package ch1;
 
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
 
 
         System.out.println("");
 
-        SQLOutput
+
         System.out.println("Cherry pick");
 
     }
