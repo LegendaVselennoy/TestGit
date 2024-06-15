297c8e9a24fb1e4ad52ea2b24c0f7cb3de8fc29a
LegendaVselennoy <rom.ahmedhanov@yandex.ru>
2024-06-15 19:15:52 +0300

    Cancle

--- src/main/java/ch1/Main.java
+++ src/main/java/ch1/Main.java
@@ -1,12 +1,14 @@
 package ch1;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
 
         System.out.println("");
 
-
+        SQLOutput
         System.out.println("Cherry pick");
 
     }
